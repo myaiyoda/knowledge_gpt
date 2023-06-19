4479e07dc602dc49dd5ef3a252b34ef7947ffa3f
Myaiyoda <myaiyoda@gmail.com>
2023-06-18 19:30:35 -0700

    Edited sidebar txt to try and commit changes

--- knowledge_gpt/components/sidebar.py
+++ knowledge_gpt/components/sidebar.py
@@ -37,7 +37,7 @@ def sidebar():
             "You can contribute to the project on [GitHub](https://github.com/mmz-001/knowledge_gpt) "  # noqa: E501
             "with your feedback and suggestions💡"
         )
-        st.markdown("Made by [mmz_001](https://twitter.com/mm_sasmitha)")
+        st.markdown("Forked by SK from [mmz_001](https://twitter.com/mm_sasmitha), to try this proj.")
         st.markdown("---")
 
         faq()
